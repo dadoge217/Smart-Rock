8e140daffc8cf2a0ed67101eacc17a535b637da1
benpurdum <bpurdum@kent.edu>
2026-03-28 15:24:21 -0400

    Added state reading, form reading, commands list, etc.

--- app.py
+++ app.py
@@ -3,11 +3,23 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route("/", methods=['GET', 'POST'])
-def hello_world():
+def home():
+    mood = "Happy"
+    status = "Not Connected"
+    companionMode = "ON"
     if request.method == 'GET':
-        return render_template('index.html')
+        return render_template('index.html', dstatus=status, dmood = mood, dcomp = companionMode)
     if request.method == 'POST':
-        username = request.form['username']
-        print(username)
-        return render_template('index.html')
+        mode = request.form['connection_mode']
+        ip = request.form['ip']
+        if mode == 'motor':
+            ip="M"+ip
+        if mode == 'wifi':
+            ip="W"+ip
+        print(ip)
+        status = "Connected!"
+        return render_template('index.html', dstatus = status, dmood = mood, dcomp = companionMode)
 
+@app.route("/menu")
+def menu():
+    return render_template('commands.html')
