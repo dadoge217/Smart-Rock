45f992ab1801bcc1651edc26c4f1dd0914ef417b
Colin <crgrant24@gmail.com>
2026-03-28 16:11:05 -0400

    finished info storing for new devices

--- commandCenter/arduinoCode.cpp
+++ commandCenter/arduinoCode.cpp
@@ -1,48 +1,149 @@
 #include <Arduino.h>
+#include <vector>
+
+class ConnectedDevice;
+
 
 class ConnectedDevice{
 public:
+    ConnectedDevice(){}
     void connectDevice(String info);
-    void runDevice();
+    void runDevice(String info);
+    int getPin(){return pin;}
+    String getIP(){return ip;}
 private:
-    String ip;
-    int pin;
-    String state;
-    String type;
+    String ip = "";
+    int pin = 0;
+    String state = "";
+    String type = "";
 };
 
+std::vector<ConnectedDevice> connectedDevices;
+
 void ConnectedDevice::connectDevice(String info){ //Format: ConnectionType.ip/pin.state
+    char charPreviousCommand = '\0';
+    String tempNum; //For storing pinNumber
     for(int i = 0; i < info.length(); i++){
-        String tempNum; //For storing pinNumber
         char value = info.charAt(i); //Get current char
-        char charPreviousCommand;
         if(value >= 'A' && value <= 'Z'){
             switch(value){
                 case 'M': //Motor (manually plugged in)
                 charPreviousCommand = 'M';
-                type = value;
+                type = String(value);
                 break;
+
                 case 'W': //WiFi (connected via ip)
                 charPreviousCommand = 'W';
-                type = value;
+                type = String(value);
                 break;
             }
         }
-        else{
+        else{ //If not uppercase (for ip/pin or misc)
+          tempNum += value; //Add current char
             switch(charPreviousCommand){
-                tempNum += value;
-                case 'M':
-                if(info.charAt(i+1) >= 'A' && info.charAt(i+1) <= 'Z'){
-                    pin = tempNum.toInt();
+                case 'M': //If device type is motor
+                if (i + 1 >= info.length() || isUpperCase(info.charAt(i+1))) {
+                  int tempPin = tempNum.toInt();
+                  bool exists = false;
+                  for(ConnectedDevice& connectedDevice : connectedDevices){
+                    if((connectedDevice.getPin() == tempPin)){
+                      exists = true;
+                      break;
+                    }
+                  }
+                  if(!exists){
+                    pin = tempNum.toInt(); //Get and prep pin
                     pinMode(pin, OUTPUT);
+                  }
                 }
                 break;
-                case 'Z':
-                if(info.charAt(i+1) >= 'A' && info.charAt(i+1) <= 'Z'){
-                    ip = tempNum;
+                case 'W': //If wifi connected
+                if (i + 1 >= info.length() || isUpperCase(info.charAt(i+1))) {
+                  String tempIP = tempNum; //Get wifi
+                  bool exists = false;
+                  for(ConnectedDevice& connectedDevice : connectedDevices){
+                    if((connectedDevice.getIP() == tempIP)){
+                      exists = true;
+                      break;
+                    }
+                  }
+                  if(!exists){
+                    ip = tempIP; //Get and prep ip
+                  }
+                  tempNum="";
                 }
             }
         }
     }
-    state = "OFF";
+    state = "OFF"; //Don't run yet
+}
+
+void ConnectedDevice::runDevice(String info){
+}
+
+void interpretCommand(String info){
+  char value = info.charAt(0);
+  if (value == 'P'){
+    ConnectedDevice newDevice;
+    newDevice.connectDevice(info);
+    connectedDevices.push_back(newDevice);
+  }
+  else if (value == 'R'){
+    char charPreviousCommand = '\0';
+    for(int i = 0; i < info.length(); i++){
+        String tempNum; //For storing pinNumber
+        char value = info.charAt(i); //Get current char
+        if(value >= 'A' && value <= 'Z'){
+            switch(value){
+                case 'M': //Motor (manually plugged in)
+                charPreviousCommand = 'M';
+                break;
+                
+                case 'W': //WiFi (connected via ip)
+                charPreviousCommand = 'W';
+                break;
+            }
+        }
+        else{
+          tempNum += value;
+            switch(charPreviousCommand){
+                case 'M':
+                if (i + 1 >= info.length() || isUpperCase(info.charAt(i+1))) {
+                    for(ConnectedDevice& connectedDevice : connectedDevices){
+                      int num = tempNum.toInt();
+                      if(connectedDevice.getPin() == num){
+                        //Run whatever you want
+                      }
+                    }
+                }
+                break;
+                case 'W':
+                if (i + 1 >= info.length() || isUpperCase(info.charAt(i+1))) {
+                    for(ConnectedDevice& connectedDevice : connectedDevices){
+                      if(connectedDevice.getIP() == tempNum){
+                        //Run whatever you want
+                      }
+                    }
+                }
+            }
+        }
+      tempNum = "";
+    }
+  }
+}
+
+void setup(){
+  Serial.begin(115200);
+  interpretCommand("PW123.234.10.5");
+  interpretCommand("PW123.234.10.5");
+  interpretCommand("PW123.234.10.6");
+  interpretCommand("PM12");
+  for(ConnectedDevice& connectedDevice : connectedDevices){
+    Serial.println("Connected Device IPs:");
+    Serial.println(connectedDevice.getIP());
+    Serial.println(connectedDevice.getPin());
+  }
+}
+void loop(){
+
 }
