1dcccc31d811c25df153a6de41c4e6f4f0f5e3ef
Colin <crgrant24@gmail.com>
2026-03-28 19:24:59 -0400

    working on brain

--- commandCenter/brain.py
+++ commandCenter/brain.py
@@ -1,6 +1,24 @@
+from RealtimeSTT import AudioToTextRecorder
+
+wake_word="Smart Rock"
+
 class SmartRockBrain:
-    def __init__(self, port):
+    def __init__(self, port = None):
         self.port = port
-    def send_command():
-        slice
-    def 
+        self.state = "IDLE"
+    def idle_mode(self):
+        #idle
+    def active_mode(self):
+        #stuff
+    def run(self):
+        if(self.state == "IDLE"):
+            self.idle_mode()
+        elif(self.state == "ACTIVE"):
+            self.active_mode()
+
+
+if __name__ == '__main__':
+    smartRock = SmartRockBrain()
+    recorder = AudioToTextRecorder()
+    while True:
+        smartRock.run()
